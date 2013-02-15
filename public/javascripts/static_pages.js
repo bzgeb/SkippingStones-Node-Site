1e2f93bff4324ef986dd7320e4e262d97cbe7566
Bronson Zgeb <bronson.zgeb@gmail.com>
2013-02-15 12:38:58 -0500

    Switched in the old site's assets

--- public/javascripts/static_pages.js
+++ public/javascripts/static_pages.js
@@ -1,9 +1,36 @@
-(function() {
+var audio = {};
 
-  $("#mail-form").bind("ajax:complete", function(xhr, status) {
-    return $("#response").html(status.responseText);
-  }).bind("ajax:beforeSend", function() {
-    return $("#response").html("<p>Sending...</p>");
-  });
+$(document).ready(function() {
+//    audio.element = document.createElement('audio');
+//    audio.element.src = Modernizr.audio.ogg ? "/assets/" + audio_id + ".ogg" :
+//                        Modernizr.audio.mp3 ? "/assets/" + audio_id + ".mp3" :
+//                                              "/assets/" + audio+id + ".m4a";
+//
+    $("#stones img").mouseenter(function() {
+        $(this).addClass("hover");
+        console.log(this);
+        audio_id = jQuery.attr(this, "data-sound")
+        e = $("#"+ audio_id)[0];
+        if(Modernizr.audio && e != undefined) {
+            e.src = Modernizr.audio.ogg ? "/sounds/" + audio_id + ".ogg" :
+                    Modernizr.audio.mp3 ? "/sounds/" + audio_id + ".mp3" :
+                                          "/sounds/" + audio+id + ".m4a";
+            e.play();
+        }
+    });
 
-}).call(this);
+    $("#stones img").mouseleave(function() {
+        $(this).removeClass("hover");
+    });
+
+    $("#stones img").click(function() {
+        audio_id = jQuery.attr(this, "data-sound")
+        e = $("#"+ audio_id)[0];
+        if(Modernizr.audio && e != undefined) {
+            e.src = Modernizr.audio.ogg ? "/sounds/" + audio_id + ".ogg" :
+                    Modernizr.audio.mp3 ? "/sounds/" + audio_id + ".mp3" :
+                                          "/sounds/" + audio+id + ".m4a";
+            e.play();
+        }
+    });
+});
